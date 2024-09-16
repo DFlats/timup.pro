66d6758d55b0ed93711bd37d581f3a840d0421b6
Oscar Hultén <oshulten@gmail.com>
2024-09-16 22:18:00 +0200

    feat: add clerk

--- Frontend/src/components/Dashboard.tsx
+++ Frontend/src/components/Dashboard.tsx
@@ -1,9 +1,19 @@
 /* eslint-disable react/react-in-jsx-scope */
 
+import { SignedOut, SignInButton } from "@clerk/clerk-react";
+
 export default function Dashboard() {
     return (
         <div className="p-4 w-screen bg-slate-400 items-center justify-center">
-            <p>Dashboard</p>
+            <SignedOut>
+                <div className="ml-auto">
+                    <SignInButton mode='modal'>
+                        <button className="btn">
+                            Sign In
+                        </button>
+                    </SignInButton>
+                </div>
+            </SignedOut>
         </div>
     );
 }
--- Frontend/src/routes/__root.tsx
+++ Frontend/src/routes/__root.tsx
@@ -2,16 +2,19 @@
 import { createRootRoute, Navigate, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import Dashboard from '../components/Dashboard'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 export const Route = createRootRoute({
     component: () => (
         <>
-            <Navigate to="/home" />
-            <Dashboard />
-            <div className="p-12 w-screen flex flex-col items-center justify-center">
-                <Outlet />
-            </div>
-            <TanStackRouterDevtools />
+            <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY} afterSignOutUrl="/home">
+                <Navigate to="/home" />
+                <Dashboard />
+                <div className="p-12 w-screen flex flex-col items-center justify-center">
+                    <Outlet />
+                </div>
+                <TanStackRouterDevtools />
+            </ClerkProvider>
         </>
     ),
 })
