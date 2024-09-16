a43a08364a083be685a261910bf28d4ee2c9e828
Oscar Hultén <oshulten@gmail.com>
2024-09-16 22:09:50 +0200

    feat: add Dashboard

---
+++ Frontend/src/components/Dashboard.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+export default function Dashboard() {
+    return (
+        <div className="p-4 w-screen bg-slate-400 items-center justify-center">
+            <p>Dashboard</p>
+        </div>
+    );
+}
--- Frontend/src/routes/__root.tsx
+++ Frontend/src/routes/__root.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { createRootRoute, Navigate, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import Dashboard from '../components/Dashboard'
 
 export const Route = createRootRoute({
     component: () => (
         <>
             <Navigate to="/home" />
+            <Dashboard />
             <div className="p-12 w-screen flex flex-col items-center justify-center">
                 <Outlet />
             </div>
