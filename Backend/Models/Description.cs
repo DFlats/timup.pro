44f2b360922c2ae0eb35a839fd5ecde10ca34121
rorychatt <pavel.rinne@gmail.com>
2024-09-16 18:13:12 +0200

    info: refactoring of code

--- Backend/Models/Description.cs
+++ Backend/Models/Description.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Backend.Models
-{
+namespace Backend.Models;
     public class Description
     {
         [Key]
@@ -14,4 +9,3 @@ namespace Backend.Models
         [Required]
         public string Text {get; set;} = null!;
     }
-}
--- Backend/Models/Tag.cs
+++ Backend/Models/Tag.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Backend.Models
+namespace Backend.Models;
+public class Tag
 {
-    public class Tag
-    {
-        [Key]
-        public int Id {get; set;}
-        [Required]
-        public string TagValue {get; set;} = null!;
-        public List<Project> Projects {get;} = [];
-    }
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string TagValue { get; set; } = null!;
+    public List<Project> Projects { get; } = [];
 }
