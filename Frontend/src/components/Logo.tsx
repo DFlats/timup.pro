96f83cf1fe67f45ac1295c8685631f1e7addd55c
timabloom <timabloom@proton.me>
2024-09-17 10:34:21 +0200

    feat: add logo

---
+++ Frontend/src/components/Logo.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+export default function Logo() {
+    return (
+        <div className="flex justify-between gap-1">
+            <p className="font-bold text-white text-4xl">Team</p>
+            <p className="font-['Atma'] font-medium text-3xl text-yellow-500 relative -top-4 -left-1">UP</p>
+        </div>
+    );
+}
