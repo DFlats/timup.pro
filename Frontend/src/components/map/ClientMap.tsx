d52f06eaf8a6a5d05a629590c6224cdae03bf343
Oscar Hultén <oshulten@gmail.com>
2024-09-17 12:23:32 +0200

    feat: setup Google Maps and migrate code

---
+++ Frontend/src/components/map/ClientMap.tsx
@@ -0,0 +1,54 @@
+import { MapProps, Map } from '@vis.gl/react-google-maps';
+
+import useClientUser from '../../hooks/useClientUser';
+import useRemoteUsers from '../hooks/useRemoteUsers';
+import useMeetings from '../hooks/useMeetings';
+
+import RemoteUserMarker, { RemoteUserMarkerProps } from './RemoteUserMarker';
+import ClientUserMarker from './ClientUserMarker';
+import Direction from './Direction';
+
+import { UserLocation } from '../types/types';
+import ConfirmedMeetingMarker from './ConfirmedMeetingMarker';
+import SuccessfulMeetingModal from './SuccessfulMeetingModal';
+
+/* eslint-disable react/react-in-jsx-scope */
+
+export default function ClientMap() {
+    const { clientUser } = useClientUser();
+    const { remoteUserLocations } = useRemoteUsers();
+    useMeetings();
+
+    if (!clientUser) {
+        console.log("clientUser is undefined");
+        return <p>Getting client user</p>
+    }
+
+    const mapProps: MapProps = {
+        defaultCenter: clientUser.location,
+        defaultZoom: 14,
+        gestureHandling: 'greedy',
+        disableDefaultUI: true,
+        mapId: import.meta.env.VITE_GOOGLE_MAP_ID,
+    }
+
+    const remoteUserMarkerProps = (location: UserLocation) => {
+        return {
+            remoteLocation: location,
+        } as RemoteUserMarkerProps;
+    }
+
+    return (
+        <>
+            <Map {...mapProps} className="w-full flex-auto">
+                {remoteUserLocations && remoteUserLocations.map(userLocation =>
+                    <RemoteUserMarker key={userLocation.user.id} {...remoteUserMarkerProps(userLocation)} />)
+                }
+                <ConfirmedMeetingMarker />
+                <ClientUserMarker />
+                <Direction />
+            </Map>
+            <SuccessfulMeetingModal />
+        </>
+    );
+}
