0e3f597e14b1486fa40eb206bf6682b69f946ea5
rorychatt <pavel.rinne@gmail.com>
2024-09-17 11:51:48 +0200

    feat: update dto structure

--- Backend/Dtos/ProjectResponse.cs
+++ Backend/Dtos/ProjectResponse.cs
@@ -2,13 +2,14 @@ using Backend.Models;
 
 namespace Backend.Dtos;
 
-public record ProjectResponse(int Id, string Title, string AuthorName, string AuthorId, (string, string)[] Collaborators, string Description, string[] Tags, bool IsCompleted)
+public record ProjectResponse(int Id, string Title, string AuthorName, string AuthorId, (string, string)[] Collaborators, string Description, string[] SkillTags, string[] InterestTags, bool IsCompleted)
 {
     public static implicit operator ProjectResponse(Project project)
     {
 
         var collaborators = project.Collaborators.Select(c => (c.Name, c.ClerkId)).ToArray();
-        var tags = project.Description.Tags.Select(t => t.TagValue).ToArray();
+        var skillTags = project.Description.SkillTags.Select(t => t.TagValue).ToArray();
+        var interestTags = project.Description.InterestTags.Select(t => t.TagValue).ToArray();
 
         return new ProjectResponse(
             project.Id,
@@ -17,7 +18,8 @@ public record ProjectResponse(int Id, string Title, string AuthorName, string Au
             project.AuthorId,
             collaborators,
             project.Description.Text,
-            tags,
+            skillTags,
+            interestTags,
             project.Progress.IsCompleted
         );
 
