aeb715ed2ecd51e7d7da9218aa16ca00cd8430fd
timabloom <timabloom@proton.me>
2024-09-17 17:04:07 +0200

    feat: implement create project form

---
+++ Frontend/src/components/CreateProjectForm.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import { SubmitHandler, useForm } from "react-hook-form"
+
+type Inputs = {
+    title: string
+    description: string
+}
+
+export function CreateProjectForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>()
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label className="pt-8 pb-1" htmlFor="name">Title* {errors.title && <span>This field is required</span>}</label>
+            <input className="input input-bordered input-primary w-full mb-6" {...register("title", { required: true })} />
+
+            <label className="pt-8 pb-1" htmlFor="name">Description* {errors.description && <span>This field is required</span>}</label>
+            <input className="input input-bordered input-primary w-full mb-10" {...register("description", { required: true })} />
+            
+            <button className="btn btn-primary mb-4 w-full" type="submit">Submit</button>
+        </form>
+    )
+}
--- Frontend/src/components/CreateProjectModal.tsx
+++ Frontend/src/components/CreateProjectModal.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 
+import { CreateProjectForm } from "./CreateProjectForm";
+
 export function CreateProjectModal() {
     return (
         <dialog id="create-project" className="modal">
             <div className="modal-box">
-                <h3 className="font-bold text-lg">Create Project</h3>
+                <h3 className="font-bold text-3xl text-center p-8">Create Project</h3>
+                <CreateProjectForm />
             </div>
             <form method="dialog" className="modal-backdrop">
                 <button>close</button>
