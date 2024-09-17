c554240d07a25a49c9ddd30dba552bf93b200c0a
timabloom <timabloom@proton.me>
2024-09-17 13:20:26 +0200

    fix: remove navigate component

--- Frontend/src/routes/__root.tsx
+++ Frontend/src/routes/__root.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { createRootRoute, Navigate, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
@@ -8,8 +8,7 @@ import Header from '../layouts/Header'
 export const Route = createRootRoute({
     component: () => (
         <>
-            <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY} afterSignOutUrl="/home">
-                <Navigate to="/" />
+            <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY} afterSignOutUrl="/">
                 <Header />
                 <div className="p-12 w-screen flex flex-col items-center justify-center">
                     <Outlet />
