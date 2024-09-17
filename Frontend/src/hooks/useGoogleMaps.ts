7ee27e55f234d37497ae2095c33ffa9276397971
Oscar Hultén <oshulten@gmail.com>
2024-09-17 12:26:47 +0200

    feat: add useGoogleMaps hook

---
+++ Frontend/src/hooks/useGoogleMaps.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+
+export default function useGoogleMaps() {
+    const queryKey = ["placesLibrary"];
+
+    useQuery({
+        queryKey: queryKey,
+        queryFn: async (): Promise<google.maps.PlacesLibrary> => {
+            return await google.maps.importLibrary("places") as google.maps.PlacesLibrary;;
+        },
+    });
+}
