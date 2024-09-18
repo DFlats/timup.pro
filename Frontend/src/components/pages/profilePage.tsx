451c9d393c7874d66887632cb4c40d8bfe9ce83a
Oscar Hultén <oshulten@gmail.com>
2024-09-18 19:00:33 +0200

    feat: add branching on profile page

--- Frontend/src/components/pages/profilePage.tsx
+++ Frontend/src/components/pages/profilePage.tsx
@@ -1,18 +1,31 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { getRouteApi } from "@tanstack/react-router";
-import { useUser } from "../../hooks";
+import { useClientUser, useUser } from "../../hooks";
 import { TagContainer } from "../../components/tags";
 
 export function ProfilePage() {
     const Route = getRouteApi('/profile/$userId');
     const { userId } = Route.useParams();
     const { user } = useUser(userId);
+    const { clientUser } = useClientUser();
 
     if (!user) return;
 
-    return (<>
-        <h1>{user.name}</h1>
-        <TagContainer tags={user.skillTags} tagType='skill' />
-        <TagContainer tags={user.interestTags} tagType='interest' />
-    </>)
+    if (clientUser && user.id == clientUser.id) {
+        return (
+            <>
+                <h1>{user.name}</h1>
+                <TagContainer tags={user.skillTags} tagType='skill' />
+                <TagContainer tags={user.interestTags} tagType='interest' />
+            </>
+        );
+    }
+
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <TagContainer tags={user.skillTags} tagType='skill' />
+            <TagContainer tags={user.interestTags} tagType='interest' />
+        </>
+    );
 }
