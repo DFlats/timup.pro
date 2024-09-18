e3a647726c281ecb748d33b0ff5708d5ec67612e
rorychatt <pavel.rinne@gmail.com>
2024-09-18 10:02:03 +0200

    feat: add getprojects call

--- Frontend/src/api/endpoints.ts
+++ Frontend/src/api/endpoints.ts
@@ -1,5 +1,13 @@
 import { paths } from './schema';
 import createClient from "openapi-fetch";
+import { ProjectFilter } from './types';
 
 const client = createClient<paths>({ baseUrl: 'http://localhost:5055' });
 
+export const getProjects = async (filter: ProjectFilter) => {
+    const response = await client.GET('/api/Projects', {
+        body: filter
+    })
+
+    return response.data;
+}
