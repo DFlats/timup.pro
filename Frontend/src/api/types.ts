d23a92638d93ecb276c7e6bdc01e6e99d8283e04
Oscar Hultén <oshulten@gmail.com>
2024-09-18 16:54:10 +0200

    feat: update api types

--- Frontend/src/api/types.ts
+++ Frontend/src/api/types.ts
@@ -1,6 +1,11 @@
 import { components } from './schema';
 
 export type Project = Required<components['schemas']['ProjectResponse']>
+export type ProjectRequest = Required<components['schemas']['ProjectRequest']>
+
 export type TagRequest = Required<components['schemas']['TagRequest']>
+
 export type User = Required<components['schemas']['UserResponse']>
-export type ValueTupleOfStringAndString = Required<components['schemas']['ValueTupleOfStringAndString']>
+export type UserRequest = Required<components['schemas']['UserRequest']>
+
+export type IdName = Required<components['schemas']['ValueTupleOfStringAndString']>
