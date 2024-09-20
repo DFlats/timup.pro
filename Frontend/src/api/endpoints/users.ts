36e046310bc46138e44075e70148986a22121d1c
Oscar Hultén <oshulten@gmail.com>
2024-09-20 21:41:45 +0200

    feat: sync users endpoints

--- Frontend/src/api/endpoints/users.ts
+++ Frontend/src/api/endpoints/users.ts
@@ -1,42 +1,79 @@
 import { client } from "../client";
 import { User, mapUserResponseToUser, UserRequest, UserPatchRequest } from "../types";
 
-export const getUserByUserId = async (id: string): Promise<User> => {
-    const response = await client.GET('/api/Users/GetUserByUserId/{id}', {
-        params: { path: { id } }
-    });
-
-    if (!response.data) {
-        throw new Error(response.error);
-    }
-
-    return mapUserResponseToUser(response.data);
-}
-
-export const createUser = async (userRequest: UserRequest) => {
-    await client.POST('/api/Users/CreateUser', {
-        body: userRequest
-    });
-}
-
-export const getRecommendedUsersByProjectId = async (projectId: number) => {
-    const response = await client.GET('/api/Users/GetRecommendedUsersByProjectId/{id}', {
+export const getUsers = async (skillTags: string[] = [], interestTags: string[] = [], page?: number) => {
+    const { response, data, error } = await client.GET('/api/Users/GetUsers', {
         params: {
-            path: {
-                id: projectId
+            query: {
+                skills: skillTags,
+                interests: interestTags,
+                page,
             }
         }
     });
 
-    if (!response.data) {
-        throw new Error(response.error);
-    }
+    if (!data || (!response.ok && error))
+        throw error;
 
-    return response.data.map(userResponse => mapUserResponseToUser(userResponse));
+    return data.map(userResponse => mapUserResponseToUser(userResponse)) as User[];
+}
+
+export const getRecommendedUsersByProjectId = async (projectId: number, page: number) => {
+    const { response, data, error } = await client.GET('/api/Users/GetRecommendedUsersByProjectId/{id}', {
+        params: {
+            path: {
+                id: projectId
+            },
+            query: {
+                page
+            }
+        }
+    });
+
+    if (!data || (!response.ok && error))
+        throw error;
+
+    return data.map(userResponse => mapUserResponseToUser(userResponse));
+}
+
+export const getUserByUserId = async (id: string) => {
+    const { response, data, error } = await client.GET('/api/Users/GetUserByUserId/{id}', {
+        params: { path: { id } }
+    });
+
+    if (!data || (!response.ok && error))
+        throw error;
+
+    return mapUserResponseToUser(data);
+}
+
+export const createUser = async (userRequest: UserRequest) => {
+    const { response, error } = await client.POST('/api/Users/CreateUser', {
+        body: userRequest
+    });
+
+    if (!response.ok && error)
+        throw error;
 }
 
 export const updateUser = async (userPatchRequest: UserPatchRequest) => {
-    await client.PATCH('/api/Users/UpdateUser', {
+    const { response, error } = await client.PATCH('/api/Users/UpdateUser', {
         body: userPatchRequest
     });
+
+    if (!response.ok && error)
+        throw error;
+}
+
+export const deleteUser = async (userId: string) => {
+    const { response, error } = await client.DELETE('/api/Users/DeleteUser/{userId}', {
+        params: {
+            path: {
+                userId
+            }
+        }
+    });
+
+    if (!response.ok && error)
+        throw error;
 }
