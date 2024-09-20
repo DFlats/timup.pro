be7bc41585c0e6f6d3eeacfd5de4f49eadbb3224
Oscar Hultén <oshulten@gmail.com>
2024-09-20 13:59:18 +0200

    feat: handleJoinProjectRequestAccept

--- Frontend/src/api/types/transactions.ts
+++ Frontend/src/api/types/transactions.ts
@@ -1,8 +1,14 @@
 import { client } from '../client';
-import { components, paths } from '../schema';
 
 export const handleJoinProjectRequest = async (userId: string, projectId: number) => {
-    const response = await client.PUT('/api/Transactions/HandleJoinProjectRequest/{userId}/{projectId}', {
+    await client.PUT('/api/Transactions/HandleJoinProjectRequest/{userId}/{projectId}', {
         params: { path: { userId, projectId } }
     });
 }
+
+export const handleJoinProjectRequestAccept = async (userId: string, projectId: number) => {
+    await client.POST('/api/Transactions/HandleJoinProjectRequest/Accept/{userId}/{projectId}', {
+        params: { path: { userId, projectId } }
+    });
+}
+
