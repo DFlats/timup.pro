6cc2b22c01d80b41b3f1fe06119dfc9f937c5198
Oscar Hultén <oshulten@gmail.com>
2024-09-21 15:42:16 +0200

    feat: add returns to useTransactions

---
+++ Frontend/src/hooks/useTransactions.ts
@@ -0,0 +1,14 @@
+import { endpoints } from "../api";
+
+export function useTransactions() {
+    return {
+        joinProjectRequest: endpoints.transactions.joinProjectRequest,
+        joinProjectRequestAccept: endpoints.transactions.joinProjectRequestAccept,
+        joinProjectRequestDeny: endpoints.transactions.joinProjectRequestDeny,
+        inviteUserToProjectRequest: endpoints.transactions.inviteUserToProject,
+        inviteUserToProjectRequestAccept: endpoints.transactions.inviteUserToProjectAccept,
+        inviteUserToProjectRequestDeny: endpoints.transactions.inviteUserToProjectDeny,
+        leaveProjectRequest: endpoints.transactions.leaveProjectRequest,
+        kickUserFromProject: endpoints.transactions.kickUserFromProject,
+    }
+}
