27ee69f5a3aaffae1992b5bf9d4119d42419ea0f
timabloom <timabloom@proton.me>
2024-09-21 11:16:41 +0200

    feat: add stars to background

--- Frontend/src/components/pages/homePage.tsx
+++ Frontend/src/components/pages/homePage.tsx
@@ -4,6 +4,8 @@ import { useUsers } from "../../hooks";
 import { ProjectFeedType } from "../../types/types";
 import { ProjectFeed } from "../../components";
 import { HeroSection } from "../layouts/heroSection";
+import "./homePage.css";
+import Stars from "./stars";
 
 export function HomePage() {
     const { clientUser } = useUsers({ type: 'clientUser' });
@@ -17,6 +19,7 @@ export function HomePage() {
 
     return (
         <>
+            <Stars />
             <HeroSection />
             <ProjectFeed projectFeed={projectFeed()} />
         </>
