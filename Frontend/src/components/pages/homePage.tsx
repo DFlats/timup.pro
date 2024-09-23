32f6e413da49579025acf73be4574a49b1d1ad17
timabloom <timabloom@proton.me>
2024-09-23 10:52:50 +0200

    feat: migrate from css branch

--- Frontend/src/components/pages/homePage.tsx
+++ Frontend/src/components/pages/homePage.tsx
@@ -1,16 +1,23 @@
 /* eslint-disable react/react-in-jsx-scope */
+
+import { HeroSection } from "../layouts/heroSection";
+import "./homePage.css";
+import Stars from "../layouts/stars";
 import { SignedIn, SignedOut } from "@clerk/clerk-react";
 import { FeaturedProjects, RecommendedProjectsForClientUser } from "../projects";
 
 export function HomePage() {
+
     return (
         <>
+            <SignedOut>
+                <Stars />
+                <HeroSection />
+                <FeaturedProjects />
+            </SignedOut>
             <SignedIn>
                 <RecommendedProjectsForClientUser />
             </SignedIn>
-            <SignedOut>
-                <FeaturedProjects />
-            </SignedOut>
         </>
     );
 }
