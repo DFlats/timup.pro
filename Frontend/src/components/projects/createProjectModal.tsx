47984969e8d2555ce14de4589fdd774245ac8d51
timabloom <timabloom@proton.me>
2024-09-24 21:58:34 +0200

    feat: style create project modal

--- Frontend/src/components/projects/createProjectModal.tsx
+++ Frontend/src/components/projects/createProjectModal.tsx
@@ -6,11 +6,12 @@ export function CreateProjectModal() {
         <dialog
             id={import.meta.env.VITE_CREATE_PROJECT_MODAL_ID}
             className="modal">
-            <div className="modal-box">
+            <div className="modal-box max-w-[29rem] p-14 pt-2 bg-[#010624] border border-white border-opacity-10">
                 <h3
-                    className="font-bold text-3xl text-center p-8">
+                    className="font-bold text-xl text-white text-center pt-8">
                     Create Project
                 </h3>
+                <p className="pb-8 pt-1 text-center">Fill in the details to create a new project.</p>
                 <CreateProjectForm />
             </div>
         
