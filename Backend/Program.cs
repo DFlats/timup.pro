1a2d5215005a17680f6353a745418774cb831383
rorychatt <pavel.rinne@gmail.com>
2024-09-24 16:24:00 +0200

    1.0.9

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Backend.Database;
 
 const string applicationTitle = "TeamUp";
-const string version = "v1.0.8";
+const string version = "v1.0.9";
 
 var builder = WebApplication.CreateBuilder(args);
 
