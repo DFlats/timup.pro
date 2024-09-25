5754eeb7a2eccbb16dabfe42688e15b1c110033b
calle1212 <ch98@hotmail.se>
2024-09-25 17:05:44 +0200

    feat: make it so when a user first joins, he gets invited to up to 5 random projects

--- Backend/Controllers/UsersController.cs
+++ Backend/Controllers/UsersController.cs
@@ -59,6 +59,8 @@ public class UsersController(DatabaseContext db) : ControllerBase
     {
         var userDbResponse = db.CreateUser(userToCheck);
 
+        if(userDbResponse.DbErrorStatusCode == DbErrorStatusCodes.Ok) db.InviteUserToSomeRandomProjects(userDbResponse.User!.ClerkId);
+
         return userDbResponse.DbErrorStatusCode switch
         {
             DbErrorStatusCodes.UserAlreadyExists => Ok((UserResponse)db.GetUserById(userToCheck.ClerkId)!),
