6c3933c52527631b9debc0c7132eab7e22696f72
timabloom <timabloom@proton.me>
2024-09-25 09:33:19 +0200

    fix: type as User or undefined

--- Frontend/src/components/pages/profilePage.tsx
+++ Frontend/src/components/pages/profilePage.tsx
@@ -5,15 +5,16 @@ import { EditUserModal } from "../users";
 import { openEditUserModal } from "../../modalControllers";
 import { useClientUser, useUserById } from "../../hooks/users";
 import { ProjectsOwnedByClientUser } from "..";
+import { User } from "../../types";
 
 export function ProfilePage() {
     const Route = getRouteApi('/profile/$userId');
     const { userId } = Route.useParams() as { userId: string };
 
-    const { userById: user } = useUserById(userId);
+    const { userById: user } = useUserById(userId) as { userById: User | undefined };
     const { clientUser } = useClientUser();
 
-    if (!user || !user.id ) return;
+    if (!user) return;
 
     const userIsClient = clientUser && user.id == clientUser.id;
 
