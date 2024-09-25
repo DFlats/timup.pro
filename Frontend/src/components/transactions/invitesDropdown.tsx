0decd1b2204f385dc1d0fc41694abdf6e57aa295
Oscar Hultén <oshulten@gmail.com>
2024-09-25 14:06:02 +0200

    feat: add InvitesDropdown

---
+++ Frontend/src/components/transactions/invitesDropdown.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+export function InvitesDropdown() {
+    <div className="dropdown dropdown-bottom">
+        <div tabIndex={0} role="button" className="btn m-1">Click</div>
+        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+        </ul>
+    </div>
+}
