b591d0dcdd34a5c35900951704d7356c3b48f6b7
rorychatt <pavel.rinne@gmail.com>
2024-09-25 13:50:39 +0200

    feat: first e2e tests

---
+++ Backend.Tests/E2E/BackendShould.cs
@@ -0,0 +1,26 @@
+namespace Backend.Tests.E2E;
+
+public class BackendShould(CustomWebAppFactory factory) : IClassFixture<CustomWebAppFactory>
+{
+    private readonly HttpClient httpClient = factory.CreateClient();
+
+    [Fact]
+    public async Task GetProjects_ReturnsSuccess()
+    {
+        var response = await httpClient.GetAsync("/api/Projects/GetProjects");
+
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        Assert.NotNull(responseString);
+    }
+
+    [Fact]
+    public async Task GetUsers_ReturnsSuccess()
+    {
+        var response = await httpClient.GetAsync("/api/Users/GetUsers");
+
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        Assert.NotNull(responseString);
+    }
+}
